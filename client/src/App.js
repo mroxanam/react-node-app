cea716773e28eab240f4232afafd7b044a330dc2
mroxanam <mroxanam20@gmail.com>
2025-08-28 16:18:14 -0300

    Initial commit - Clean structure

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{!data ? "Cargando..." : data}</h1>
+    </div>
+  );
+}
+
+export default App;
