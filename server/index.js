15a6749b6701a93a9b77409922a96bb04c73f49d
mroxanam <mroxanam20@gmail.com>
2025-08-30 14:04:57 -0300

    Corregido server/index.js para deploy en Render

--- server/index.js
+++ server/index.js
@@ -10,13 +10,16 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hola desde el servidor!" });
 });
 
-// Cuando esté en producción, servir la app React
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+// Servir archivos estáticos del build de React
+const clientBuildPath = path.join(__dirname, "../client/build");
+app.use(express.static(clientBuildPath));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+// Cualquier otra ruta que no sea /api, devuelve index.html
+app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.join(clientBuildPath, "index.html"));
 });
 
+// Iniciar servidor
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
